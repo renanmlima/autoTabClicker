571074a7cb9af7f2abb7aff7ee63355dfbdbfcdd
Renan Lima <renanmslimabrasil@hotmail.com>
2022-05-25 10:21:32 -0300

    Create autotabclicker-1.1.py

---
+++ autotabclicker-1.1.py
@@ -0,0 +1,51 @@
+import pyautogui
+import keyboard
+import time
+
+'''
+ÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜÜ
+
+±±°  Programa: autoTabClicker | Autor:  Renan Lima  | Data : 15/02/2022   °±±
+
+±±ÌÍÍÍÍÍÍÍÍÍÍØÍÍÍÍÍÍÍÍÍÍÊÍÍÍÍÍÍÍÏÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÊÍÍÍÍÍÍÏÍÍÍÍÍÍÍÍÍÍÍÍÍ¹±±
+
+±±°  Programa com intuito de automatizar cliques após leitura de barcode  °±±
+
+±±ÌÍÍÍÍÍÍÍÍÍÍØÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¹±±
+
+±±°  Uso Amvian Industria e Comercio de Pecas Automotivas                 °±±
+
+±±ÈÍÍÍÍÍÍÍÍÍÍÏÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼±±
+
+ßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßß
+'''
+# ---- Funcao que identifica a posicao do mouse na tela
+
+'''''
+print('-------Camaco-Amvian-------') 
+
+time.sleep(8)
+posicao = pyautogui.position()
+tamanho = pyautogui.size()
+print(posicao)
+print(tamanho)
+'''''
+
+# ---- Loop que identifica o pressionamento do tab
+
+while True:
+ 
+    keyboard.wait('tab')  
+    print('Tab pressionado')  
+    time.sleep(5)
+
+    # ---- Função do clique
+    pyautogui.click(x=971, y=493)      
+
+  
+  
+                    
+
+    
+                                    
+    
